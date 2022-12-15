021aac09e643b67aeaea475e4dfbf21d73fe4057
Alexis Roques <alexisrqs@outlook.fr>
2022-12-15 11:36:28 +0100

    First commit

---
+++ src/main/java/org/waa/sme/discordo/DiscordoApplication.java
@@ -0,0 +1,13 @@
+package org.waa.sme.discordo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/waa/sme/discordo/DiscordoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.waa.sme.discordo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
