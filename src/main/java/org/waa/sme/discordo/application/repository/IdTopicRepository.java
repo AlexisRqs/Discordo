30aff10dee27927e88c8dc368d8097920cd230ae
Ninwost <pelletier.winston@gmail.com>
2023-01-09 19:41:10 +0100

    debut creation bdd

--- src/main/java/org/waa/sme/discordo/application/repository/IdTopicRepository.java
+++
@@ -1,9 +0,0 @@
-package org.waa.sme.discordo.application.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.waa.sme.discordo.infrastructure.application.model.Users;
-
-@Repository
-public interface IdTopicRepository extends CrudRepository<Users, Long> {
-}
