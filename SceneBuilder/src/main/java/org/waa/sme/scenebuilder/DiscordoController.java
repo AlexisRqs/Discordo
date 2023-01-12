7bdcde9e5affeffb64cb880f68589055ce874bc0
Alexis Roques <alexisrqs@outlook.fr>
2023-01-12 18:35:33 +0100

    Successful merge (Front + API)

--- SceneBuilder/src/main/java/org/waa/sme/scenebuilder/DiscordoController.java
+++ SceneBuilder/src/main/java/org/waa/sme/scenebuilder/DiscordoController.java
@@ -1,8 +1,10 @@
 package org.waa.sme.scenebuilder;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class DiscordoController {
@@ -42,4 +44,9 @@ public class DiscordoController {
     protected void onemojiButtonClick() {
         welcomeText.setText("Bienvenue sur Discordo !");
     }
+
+    ListView<String> list = new ListView<String>();
+    //ObservableList<String> items = FXCollections.observableArrayList (
+    //        "Single", "Double", "Suite", "Family App");
+    //list.setItems(items);
 }
