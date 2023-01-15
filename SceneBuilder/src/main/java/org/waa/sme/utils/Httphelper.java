054ad1727607ce33e9192f8ee196d11fb3489003
Alexis Roques <alexisrqs@outlook.fr>
2023-01-15 21:54:29 +0100

    Working resgister form

--- SceneBuilder/src/main/java/org/waa/sme/utils/Httphelper.java
+++ SceneBuilder/src/main/java/org/waa/sme/utils/Httphelper.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
